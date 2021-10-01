2bb112cc5a5880af99f362f0a95b65e160406f6f
Rahul Kulkarni <kdrahul@github.com>
2021-10-01 23:58:21 +0530

    Added basic functionality to Login page

--- src/App.js
+++ src/App.js
@@ -5,22 +5,12 @@ import Register from './templates/register';
 import Store from './templates/store';
 import Dashboard from './templates/dashboard';
 import Navbar from './components/navbar';
-<<<<<<< HEAD
 import { Client, resources } from 'coinbase-commerce-node';
 import { cors } from 'cors';
 
 const App = () => {
     Client.init(process.env.REACT_APP_API_KEY);
-=======
-// import { Client, resources } from 'coinbase-commerce-node';
-import { cors } from 'cors';
-import { env } from 'process';
 
-const App = () => {
-
-    console.log(env.REACT_APP_NODE_ENV);
->>>>>>> 8b3b85b1b395e7b9c548f703e89196356f68af8b
-  // const { Charge } = resources;
   return (
     <div className="container-fluid">
       <Navbar />
--- src/templates/login.js
+++ src/templates/login.js
@@ -1,55 +1,48 @@
+import { useHistory } from 'react-router-dom';
+import { useState } from 'react';
 const Login = () => {
+  const [uname, setUname] = useState('');
+  const [pass, setPass] = useState('');
+  const [isSubmitted, setIsSubmitted] = useState(false);
+    const history = useHistory();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(uname);
+    console.log(pass);
+      setIsSubmitted(true);
+      if(isSubmitted) {
+        return history.push('/');
+      }
+  };
   return (
     <div>
-      <div
-        className="modal fade"
-        id="exampleModal"
-        tabindex="-1"
-        aria-labelledby="exampleModalLabel"
-        aria-hidden="true"
-      >
-        <div className="modal-dialog">
-          <div className="modal-content">
-            <div className="modal-header">
-              <h5 className="modal-title" id="exampleModalLabel">
-                Login
-              </h5>
-              <button
-                type="button"
-                className="btn-close"
-                data-bs-dismiss="modal"
-                aria-label="Close"
-              ></button>
-            </div>
-            <div className="modal-body">
-              <form action="insert.php" method="post">
-                <div className="mb-3">
-                  <p className="form-label"> User ID</p>{' '}
-                  <input type="text" className="form-control" name="email" />
-                </div>
-                <div className="mb-3">
-                  <p className="form-label"> Pin</p>{' '}
-                  <input type="text" className="form-control" name="pw" />
-                </div>
-
-                <center>
-                  <button
-                    className="btn btn-outline-primary"
-                    type="submit"
-                  >
-                    Login
-                  </button>
-                  <button
-                    className="btn btn-outline-primary"
-                    type="submit"
-                  >
-                    Register
-                  </button>
-                </center>
-              </form>
-            </div>
+      <div className="modal-body">
+        <form onSubmit={handleSubmit}>
+          <div className="mb-3">
+            <p className="form-label"> User ID</p>
+            <input
+              type="text"
+              className="form-control"
+              name="email"
+              onChange={(e) => setUname(e.target.value)}
+            />
           </div>
-        </div>
+          <div className="mb-3">
+            <p className="form-label"> Pin</p>
+            <input
+              type="text"
+              className="form-control"
+              name="password"
+              onChange={(e) => setPass(e.target.value)}
+            />
+          </div>
+
+          <center>
+            <button className="btn btn-outline-primary" type="submit">
+              Login
+            </button>
+          </center>
+        </form>
       </div>
     </div>
   );
